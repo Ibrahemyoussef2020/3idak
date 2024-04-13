ce9bf34131ab98116bf31310bf65aeb76a350979
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-04-13 23:34:36 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const transformers = document.querySelectorAll('.tarns-observer');
+
+const observer = new IntersectionObserver(
+
+    entries => {
+        entries.forEach(entry => {
+            const element = entry.target;
+            
+            if (element.classList.contains('scale')) {
+                element.classList.toggle('scale-1', entry.isIntersecting)
+            }
+            else if(element.classList.contains('traslate')){
+                element.classList.toggle('traslate-back', entry.isIntersecting)
+            }     
+            if (entry.isIntersecting) observer.unobserve(element)  
+        }
+            
+        )
+    },
+    {
+        threshold:0,
+    }
+)
+
+transformers.forEach(transformer => {
+    observer.observe(transformer)
+})
